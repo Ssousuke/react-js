b7bb8c6c94080bc4f8ba0e5fa3be5de4eefe92e1
wesleysousuke <wesleysousukee@gmail.com>
2022-11-09 21:29:15 -0300

    classe com estado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "Matheus",
+    count: 0
+  };
+
+  handlePClick = () => {
+    this.setState({ name: "Wesley" });
+  }
+
+  hendleAClick = (event) => {
+    event.preventDefault();
+    const { count } = this.state;
+    this.setState({ count: count + 1 });
+  }
+
+  render() {
+    const { name, count } = this.state;
+
+    return (
+      <div>
+        <h1 onClick={this.handlePClick}>{name}</h1>
+        <h1 onClick={this.hendleAClick}>{count}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
